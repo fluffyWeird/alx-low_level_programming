5c754ccbd9d86729182a12978a23c401ca03a9c3
fluffyweird <eyuduc@gmail.com>
2023-03-09 17:01:31 +0300

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,8 @@
 int main(void)
 {
 char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char ch2[] = "\n";
 write(2, ch, sizeof(ch));
+write(2, ch2, sizeof(ch2));
 return (0);
 }
