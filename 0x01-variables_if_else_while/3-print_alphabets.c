aba28d48722ce25bdde819dc504d6e226d7e90fc
fluffyweird <eyuduc@gmail.com>
2023-03-11 22:58:53 +0300

    done

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,28 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
+#include <limits.h>
 /**
- * main - main block
- * Description:  prints the alphabet in lowercase,
- * and then in uppercase, followed by a new line.
+ * main - main block print
+ * Description: to print alphabets from the ANSI and it is really
  * Return: 0
- */
+*/
 int main(void)
 {
-	char c = 'a';
-
-	while (c <= 'z')
-	{
-		putchar(c);
-		c++;
-	}
-
-	c = 'A';
-
-	while (c <= 'Z')
-	{
-		putchar(c);
-		c++;
-	}
-
-	putchar('\n');
+int n = 'a';
+while (n <= 'z')
+{
+putchar(n);
+if (n == 'z')
+{
+n = 'A';
+while (n <= 'Z')
+{
+putchar(n);
+if (n == 'Z')
+{
+n = 'z';
+break;
+}
+n++;
+}
+putchar('\n');
+}
+n++;
+}
 	return (0);
 }
