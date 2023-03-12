382daf8b859b9be3f041787a6a546b13bd4cf993
fluffyweird <eyuduc@gmail.com>
2023-03-12 11:24:09 +0300

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,8 +3,6 @@
 /**
  * putcharo - writes the character c to stdout
  * @c: The character to print
- * main - main function printout the doc
- * Description : main will take the void putcharo and print it or return 0
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -12,6 +10,11 @@ void putcharo(char c[])
 {
 printf("%s\n", c);
 }
+/**
+ * main - main function printout the doc
+ * Description : main will take the void putcharo and print it or return 0
+ * Return: Alwasy 0(success)
+ */
 int main(void)
 {
 	putcharo("_putchar");
