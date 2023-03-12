cdb6173c2d48738dad507e38bfd97b9aa49c859c
fluffyweird <eyuduc@gmail.com>
2023-03-12 12:14:20 +0300

    doen

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,26 @@
-#include "holberton.h"
-
+#include <stdio.h>
 /**
- * print_alphabet - Entry point
- * Return:0
- */
-
+ * print_alphabet- prints alphabets
+ * Description: printing alphabets
+ * returns alphabet
+*/
 void print_alphabet(void)
 {
-	char c = 'a';
-
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
-
-	}
-	_putchar('\n');
-
+char c = 'a';
+while (c < 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+}
+/**
+ * main- main function
+ * Description: printing alphabets
+ * Return: 0;
+*/
+int main(void)
+{
+print_alphabet();
+return (0);
 }
