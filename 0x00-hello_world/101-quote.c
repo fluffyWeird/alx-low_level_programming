bf3673ff45bdd4a009f25ac4bd11fa8b68685100
fluffyweird <eyuduc@gmail.com>
2023-03-12 10:31:20 +0300

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
+#include <limits.h>
 #include <stdio.h>
-#include <unistd.h>
-/**
- * main - Entry point
- * Return:1
- */
-int main(void)
-{
-char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-char ch2[] = "\n";
-write(2, ch, sizeof(ch));
-write(2, ch2, sizeof(ch2));
-write(2, ch2, sizeof(ch2));
-return (0);
+#include <stdlib.h>
+
+int main(){
+      char c;
+
+      c = CHAR_MIN;
+      while(c != CHAR_MAX){
+              printf("%d\n", c);
+              c = c+1;
+      }
+
+      exit(EXIT_SUCCESS);
 }
