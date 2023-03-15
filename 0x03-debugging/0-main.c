aa862516c1a2cfaab72e866620b7e772c94e2375
fluffyweird <eyuduc@gmail.com>
2023-03-15 16:50:34 +0300

    doen

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,6 +4,25 @@
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
+void positive_or_negative(int n)
+{
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
 
 int main(void)
 {
