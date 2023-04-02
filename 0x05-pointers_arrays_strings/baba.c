a9c29d0be9a5bb9df1ce05f9c7de5e826987dc68
fluffyweird <eyuduc@gmail.com>
2023-04-02 10:08:31 +0300

    done

---
+++ 0x05-pointers_arrays_strings/baba.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int _strlen(char *s){
+   int i, j;
+   for (i=0; i<=sizeof(*s); i++){
+        if(i==sizeof(*s){
+            j=i;
+        })
+   }
+   return j;
+}
