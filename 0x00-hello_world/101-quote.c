2078aa40e377c5a6a15aff035173fb8989f760d0
fluffyweird <eyuduc@gmail.com>
2023-04-02 10:04:06 +0300

    doen

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,21 @@
-#include <limits.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
-int main(){
-      char c;
-
-      c = CHAR_MIN;
-      while(c != CHAR_MAX){
-              printf("%d\n", c);
-              c = c+1;
-      }
-
-      exit(EXIT_SUCCESS);
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
+int main(void)
+{
+char b[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int i;
+for (i = 0; i < sizeof(b); i++)
+_putchar(b[i]);
+}
+
