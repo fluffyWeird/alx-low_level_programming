226b798b5b33f323e41ba3981bcb32e1cc8f72a9
fluffyweird <eyuduc@gmail.com>
2023-04-07 23:50:32 +0300

    done

--- 0x05-pointers_arrays_strings/baba.c
+++ 0x05-pointers_arrays_strings/baba.c
@@ -1,11 +1,22 @@
+// size_t print_list(const list_t *h){
+
+// }
+
 #include <stdio.h>
-#include <string.h>
-int _strlen(char *s){
-   int i, j;
-   for (i=0; i<=sizeof(*s); i++){
-        if(i==sizeof(*s){
-            j=i;
-        })
-   }
-   return j;
+#include <stdlib.h>
+
+typedef struct guzo{
+    char *place;
+    char *leavingTime;
+    struct guzo *next;
+}guzo;
+int main(){
+    guzo debub={"Arbaminch", "March", NULL};
+    guzo amara={"Gonder", "April", NULL};
+    guzo harer={"sebategna","June", NULL};
+
+    debub.next=&amara;
+    amara.next=&harer;
+
+    display(guzo guzo);
 }
---
+++ 0x05-pointers_arrays_strings/chacha.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct island {
+ char *name;
+ char *opens;
+ char *closes;
+ struct island *next;
+} island;
+int main()
+{
+island amity = {"Amity", "09:00", "17:00", NULL};
+island craggy = {"Craggy", "09:00", "17:00", NULL};
+island isla_nublar = {"Isla Nublar", "09:00", "17:00", NULL};
+island shutter = {"Shutter", "09:00", "17:00", NULL};
+amity.next = &craggy;
+craggy.next = &isla_nublar;
+isla_nublar.next = &shutter;
+island skull = {"Skull", "09:00", "17:00", NULL};
+isla_nublar.next = &skull;
+skull.next = &shutter;
+display(&amity);
+}
