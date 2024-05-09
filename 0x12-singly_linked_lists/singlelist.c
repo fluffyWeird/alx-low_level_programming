e5328aa620f83f1528ee047cd96290bac4b24ea3
fluffyweird <ets027912feng@aastu.edu.et>
2024-05-10 02:32:26 +0300

    part 2- Understood how to itterate through a linked list

--- 0x12-singly_linked_lists/singlelist.c
+++ 0x12-singly_linked_lists/singlelist.c
@@ -2,22 +2,41 @@
 #include <stdlib.h>
 
 // typedef char charina[10];
-typedef struct Node{
+
+typedef struct Node Node;
+struct Node{
     int x;
-    struct Node *next;
-}Node;
+    Node *next;
+};
+
 int main(){
- Node root, element2;
- root.x=10;
-//  root.next= &element2;
-//  element2.x=11;
-//  element2.next=NULL;
-// printf("%d", element2.x)
+    Node root;
+    root.x=10;
 
-/**So here I understood how the root thing is called*/
-// root.next=malloc(sizeof(Node));
-// root.next -> x=11;
-// root.next -> next=NULL;
-// printf("%d", root.next->x);
+    root.next = malloc(sizeof(Node));
+    root.next->x=11;
+    root.next->next=NULL;
 
+    root.next->next= malloc(sizeof(Node));
+    root.next->next->x=12;
+    root.next->next->next=NULL;
+    printf("root.x = %d", root.next->next->x);
+    Node* curr= &root;
+
+    /**Itterating using Loops I freaking love it*/
+    while (curr != NULL){
+         if(curr->next==NULL){
+            printf("%d", curr->x);
+        }
+        curr=curr->next;
+       
+    }
+    for(Node* cury=&root; cury != NULL; cury= cury->next){
+        printf("%d\n", cury->x);
+    }
+ 
+
+free(root.next->next);
+free(root.next);
 }
+
